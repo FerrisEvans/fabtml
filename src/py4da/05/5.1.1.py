0921d711a6e70bc381a865411707401f88eaa593
Ferris <ferris.evans@outlook.com>
2025-07-31 23:02:01 +0800

    5.1.1

--- src/py4da/05/5.1.1.py
+++ src/py4da/05/5.1.1.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
 
@@ -16,3 +17,8 @@ print(s)
 
 s1 = Series(s, ['a', 'c'])
 print(s1)
+
+s = Series([1, 2, np.nan, 3, 4, None, 5], index=['a', 'b', 'c', 'd', 'e', 'f', 'g'], name='data')
+print(f"默认取前 5 行数据\n {s.head(3)}")
+print(f"默认取后 5 行数据\n {s.tail(2)}")
+print(f"查看所有描述性信息\n {s.describe()}")
